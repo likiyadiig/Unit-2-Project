d0d6bd343b1709fd532a2516b48d1edb20af3fcf
twitchout <twitchoutt@gmail.com>
2025-06-24 22:09:08 -0500

    Update session_db.php
    added 3 basic functions to session_db page

--- model/session_db.php
+++ model/session_db.php
@@ -1,5 +1,18 @@
+<!-- added function start_custom_session - starts inital session
+    function is_user_logged_in - checks if user is logged in via user_id
+    function log_out_user - logs user out and remember me   - Jeremy 6/24/25  --!>
 <?php
-// Session Database Functions
+function start_custom_session() {
+    session_start();
+}
 
-//Session: start session, validate user login, check if logged in, destroy session
+function is_user_logged_in() {
+    return isset($_SESSION['user_id']);
+}
+
+function log_out_user() {
+    session_unset();
+    session_destroy();
+    setcookie('remember_me', '', time() - 3600, '/');
+}
 ?>
