030ea9d5318dbb287b151f21c8b38f7e9a940907
Victoria <150954681+nxpeach@users.noreply.github.com>
2025-07-05 10:18:02 -0500

    add .gitignore file

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require_once('model/session_db.php');
 require_once('model/user_db.php');
 
--- model/session_db.php
+++ model/session_db.php
@@ -1,13 +1,19 @@
-<!-- added function start_custom_session - starts inital session
-    function is_user_logged_in - checks if user is logged in via user_id
-    function log_out_user - logs user out and remember me   - Jeremy 6/24/25  --!>
 <?php
+// added function start_custom_session - starts inital session
+// function is_user_logged_in - checks if user is logged in via user_id
+// function log_out_user - logs user out and remember me   - Jeremy 6/24/25
 function start_custom_session() {
     session_start();
 }
 
 function is_user_logged_in() {
     return isset($_SESSION['user_id']);
+}
+
+// Log in user by setting session variable
+function log_in_user($user) {
+    $_SESSION['user_id'] = $user['id'];
+    $_SESSION['username'] = $user['username'];
 }
 
 function log_out_user() {
